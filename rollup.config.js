a9490a353d7046514cad3fe95c71b0a1d162b140
junbin <540965117@qq.com>
2021-04-10 16:34:40 +0800

    完善

--- rollup.config.js
+++ rollup.config.js
@@ -46,7 +46,7 @@ const pluginsConfig = [
   production && terser()
 ]
 // 跨域处理
-function proxy() {
+function proxy({ port }) {
   let started = false
   return {
     writeBundle() {
@@ -55,7 +55,7 @@ function proxy() {
         // Listen on a specific host via the HOST environment variable
         var host = process.env.HOST || 'localhost'
         // Listen on a specific port via the PORT environment variable
-        var port = process.env.PORT || 8080
+        var portTemp = process.env.PORT || port
 
         var cors_proxy = require('cors-anywhere')
         cors_proxy
@@ -64,8 +64,8 @@ function proxy() {
             requireHeader: ['origin', 'x-requested-with'],
             removeHeaders: ['cookie', 'cookie2']
           })
-          .listen(port, host, function () {
-            console.log('Running CORS Anywhere on ' + host + ':' + port)
+          .listen(portTemp, host, function () {
+            console.log('Running CORS Anywhere on ' + host + ':' + portTemp)
           })
       }
     }
@@ -81,7 +81,7 @@ export default [
       name: 'index',
       file: 'public/index/index.js'
     },
-    plugins: [...pluginsConfig, css({ output: 'index.css' }), !production && proxy()],
+    plugins: [...pluginsConfig, css({ output: 'index.css' }), proxy({ port: 8080 })],
     watch: {
       clearScreen: false
     }
@@ -94,7 +94,7 @@ export default [
       name: 'popup',
       file: 'public/popup/popup.js'
     },
-    plugins: [...pluginsConfig, css({ output: 'popup.css' })],
+    plugins: [...pluginsConfig, css({ output: 'popup.css' }), proxy({ port: 8081 })],
     watch: {
       clearScreen: false
     }
@@ -107,7 +107,7 @@ export default [
       name: 'content',
       file: 'public/content/content.js'
     },
-    plugins: [...pluginsConfig, css({ output: 'content.css' })],
+    plugins: [...pluginsConfig, css({ output: 'content.css' }), proxy({ port: 8082 })],
     watch: {
       clearScreen: false
     }
@@ -120,7 +120,7 @@ export default [
       name: 'background',
       file: 'public/background/background.js'
     },
-    plugins: [...pluginsConfig, css({ output: 'background.css' })],
+    plugins: [...pluginsConfig, css({ output: 'background.css' }), proxy({ port: 8083 })],
     watch: {
       clearScreen: false
     }
