16045fd2621aa74bf1aba7fdbb3d7d31f73bad1f
junbin <540965117@qq.com>
2022-08-28 06:54:53 +0800

    build: 自动生成config.js文件

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,16 @@ import css from 'rollup-plugin-css-only'
 import scss from 'rollup-plugin-scss'
 import preprocess from 'svelte-preprocess'
 import image from '@rollup/plugin-image'
+import fs from 'fs'
+
 const production = !process.env.ROLLUP_WATCH
+
+const path = `${__dirname}/src/config.js`
+if (!fs.existsSync(path)) {
+  fs.writeFileSync(path, 'export const caiYunToken = \'oo00trx4oclspt3nqhfc\'')
+  console.log('请在config.js中配置token')
+  process.exit(0)
+}
 
 const pluginsConfig = [
   production && terser({
